7804b23b29ec02631e8360d8d1946a97aa544a44
huynth1801 <huuhuy1801@gmail.com>
2024-05-14 20:40:36 +0700

    delete middleware

--- server/src/tmdb/tmdb.api.ts
+++ server/src/tmdb/tmdb.api.ts
@@ -1,22 +1,22 @@
 import axiosClient from "../axios/axios.client";
 import tmdbEndpoints from "./tmdb.endpoints";
-import { MediaBase, MediaList, MediaDetail, MediaSearch } from "./tmdb.endpoints";
+import { MediaBase, MediaList, MediaDetail, MediaSearch, PersonType } from "./tmdb.endpoints";
 
 
 const tmdbApi = {
-    mediaList: async ({mediaType, mediaCategory, page}: MediaList) => axiosClient.get(
+    mediaList: async ({mediaType, mediaCategory, page}: MediaList) => await axiosClient.get(
         tmdbEndpoints.mediaList({mediaType, mediaCategory, page})
     ),
-    mediaDetail: async ({mediaType, mediaId}: MediaDetail) => axiosClient.get(
+    mediaDetail: async ({mediaType, mediaId}: MediaDetail) => await axiosClient.get(
         tmdbEndpoints.mediaDetail({mediaType, mediaId})
     ),
-    mediaGenres: async ({mediaType}: MediaBase) => axiosClient.get(
+    mediaGenres: async ({mediaType}: MediaBase) => await axiosClient.get(
         tmdbEndpoints.mediaGenres({mediaType})
     ),
-    mediaCredits: async ({mediaType, mediaId}: MediaDetail) => axiosClient.get(
+    mediaCredits: async ({mediaType, mediaId}: MediaDetail) => await axiosClient.get(
         tmdbEndpoints.mediaCredits({mediaType, mediaId})
     ),
-    mediaVideos: async ({mediaType, mediaId}: MediaDetail) => axiosClient.get(
+    mediaVideos: async ({mediaType, mediaId}: MediaDetail) => await axiosClient.get(
         tmdbEndpoints.mediaVideos({mediaType, mediaId})
     ),
     mediaImages: async ({mediaType, mediaId}: MediaDetail) => axiosClient.get(
@@ -27,6 +27,12 @@ const tmdbApi = {
     ),
     mediaSearch: async ({ mediaType, query, page }: MediaSearch) => axiosClient.get(
         tmdbEndpoints.mediaSearch({ mediaType, query, page })
+    ),
+    personDetail: async ({personId}: PersonType) => axiosClient.get(
+        tmdbEndpoints.personDetail({personId})
+    ),
+    personMedias: async({personId}: PersonType) => axiosClient.get(
+        tmdbEndpoints.personMedias({personId})
     )
     
 }
