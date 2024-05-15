45531ada90e17cfd2b082c3bf7ffffad1bfb0b56
huynth1801 <huuhuy1801@gmail.com>
2024-05-15 14:40:07 +0700

    add favoriate model and controller

--- server/src/tmdb/tmdb.endpoints.ts
+++ server/src/tmdb/tmdb.endpoints.ts
@@ -1,56 +1,57 @@
 import tmdb from "./tmdb";
 
 export interface MediaBase {
-    mediaType: string
+  mediaType: string;
 }
 
 export interface MediaList extends MediaBase {
-    mediaCategory: string;
-    page: number
+  mediaCategory: string;
+  page: number;
 }
 
 export interface MediaDetail extends MediaBase {
-    mediaId: number
+  mediaId: number;
 }
 
 export interface MediaSearch extends MediaBase {
-    query: string,
-    page: number
+  query: string;
+  page: number;
 }
 
 export interface PersonType {
-    personId: number
+  personId: number;
 }
 
 const tmdbEndpoints = {
-  mediaList: ({ mediaType, mediaCategory, page }: MediaList) => tmdb.getUrl(
-    `${mediaType}/${mediaCategory}`,  page.toString()
-  ),
-  mediaDetail: ({ mediaType, mediaId }: MediaDetail) => tmdb.getUrl(
-    `${mediaType}/${mediaId}`
-  ),
-  mediaGenres: ({ mediaType }: MediaBase) => tmdb.getUrl(
-    `genre/${mediaType}/list`
-  ),
-  mediaCredits: ({ mediaType, mediaId }: MediaDetail) => tmdb.getUrl(
-    `${mediaType}/${mediaId}/credits`
-  ),
-  mediaVideos: ({ mediaType, mediaId }: MediaDetail) => tmdb.getUrl(
-    `${mediaType}/${mediaId}/videos`
-  ),
-  mediaRecommend: ({ mediaType, mediaId }: MediaDetail) => tmdb.getUrl(
-    `${mediaType}/${mediaId}/recommendations`
-  ),
-  mediaImages: ({ mediaType, mediaId }: MediaDetail) => tmdb.getUrl(
-    `${mediaType}/${mediaId}/images`
-  ),
-  mediaSearch: ({ mediaType, query, page }: MediaSearch) => tmdb.getUrl(`search/${mediaType}?query=${query}&page=${page.toString()}`),
-  personDetail: ({ personId }: PersonType) => tmdb.getUrl(
-    `person/${personId}`
-  ),
-  personMedias: ({ personId }: PersonType) => tmdb.getUrl(
-    `person/${personId}/combined_credits`
-  ),
+  mediaList: ({
+    mediaType,
+    mediaCategory,
+    page,
+  }: MediaList) =>
+    tmdb.getUrl(
+      `${mediaType}/${mediaCategory}`,
+      page.toString()
+    ),
+  mediaDetail: ({ mediaType, mediaId }: MediaDetail) =>
+    tmdb.getUrl(`${mediaType}/${mediaId}`),
+  mediaGenres: ({ mediaType }: MediaBase) =>
+    tmdb.getUrl(`genre/${mediaType}/list`),
+  mediaCredits: ({ mediaType, mediaId }: MediaDetail) =>
+    tmdb.getUrl(`${mediaType}/${mediaId}/credits`),
+  mediaVideos: ({ mediaType, mediaId }: MediaDetail) =>
+    tmdb.getUrl(`${mediaType}/${mediaId}/videos`),
+  mediaRecommend: ({ mediaType, mediaId }: MediaDetail) =>
+    tmdb.getUrl(`${mediaType}/${mediaId}/recommendations`),
+  mediaImages: ({ mediaType, mediaId }: MediaDetail) =>
+    tmdb.getUrl(`${mediaType}/${mediaId}/images`),
+  mediaSearch: ({ mediaType, query, page }: MediaSearch) =>
+    tmdb.getUrl(
+      `search/${mediaType}?query=${query}&page=${page.toString()}`
+    ),
+  personDetail: ({ personId }: PersonType) =>
+    tmdb.getUrl(`person/${personId}`),
+  personMedias: ({ personId }: PersonType) =>
+    tmdb.getUrl(`person/${personId}/combined_credits`),
 };
 
 export default tmdbEndpoints;
